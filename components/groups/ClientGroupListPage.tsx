242a0394e3ec0802bcb0fa6d5de9a23053e0ae7c
dongjaekim <dongjaekim68@gmail.com>
2025-05-17 18:17:59 +0900

    fix: 불필요한 HydrationBoundary 태그 제거, 서버 컴포넌트에서만 감싸면 됨

--- components/groups/ClientGroupListPage.tsx
+++ components/groups/ClientGroupListPage.tsx
@@ -2,26 +2,17 @@
 
 import { GroupList } from "@/components/groups/GroupList";
 import { CreateGroupButton } from "@/components/groups/CreateGroupButton";
-import { DehydratedState, HydrationBoundary } from "@tanstack/react-query";
 
-interface IClientGroupListPageProps {
-  dehydratedState: DehydratedState;
-}
-
-export function ClientGroupListPage({
-  dehydratedState,
-}: IClientGroupListPageProps) {
+export function ClientGroupListPage() {
   return (
-    <HydrationBoundary state={dehydratedState}>
-      <div className="container mx-auto py-8">
-        <div className="flex items-center justify-between">
-          <h1 className="text-2xl font-bold">모임 목록</h1>
-          <CreateGroupButton />
-        </div>
-        <div className="mt-8">
-          <GroupList />
-        </div>
+    <div className="container mx-auto py-8">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">모임 목록</h1>
+        <CreateGroupButton />
       </div>
-    </HydrationBoundary>
+      <div className="mt-8">
+        <GroupList />
+      </div>
+    </div>
   );
 }
